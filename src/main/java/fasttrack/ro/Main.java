b2ccbee7687ac3d0566b7548c67bc2051aadceac
knimrod27 <nemfelejtemel14@gmail.com>
2023-12-29 19:23:45 +0200

    fist commit

---
+++ src/main/java/fasttrack/ro/Main.java
@@ -0,0 +1,8 @@
+package fasttrack.ro;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("First branching v2");
+    }
+}
