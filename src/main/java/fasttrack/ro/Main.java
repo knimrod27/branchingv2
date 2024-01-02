53138c046a93f4f750cca0028f89b12069512961
knimrod27 <nemfelejtemel14@gmail.com>
2024-01-02 18:39:53 +0200

    COMMIT FOR BRANCH asd

--- src/main/java/fasttrack/ro/Main.java
+++ src/main/java/fasttrack/ro/Main.java
@@ -1,8 +1,32 @@
 package fasttrack.ro;
 
+import java.lang.module.FindException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("First branching v2");
+        int arr[] = {2,2,3,4,5};
+        System.out.println(reverseProd(arr));
+        System.out.println(reverseProdWithFor(arr));
+
+    }
+
+    public static int reverseProd(int[] arr) {
+        int index = arr.length -1;
+        int prod = 1;
+        while(index > 0) {
+            prod *= arr[index];
+            index--;
+        }
+
+return prod;
+    }
+
+    public static int reverseProdWithFor(int[] arr) {
+        int prod = 1;
+        for(int i = 0; i< arr.length; i ++) {
+            prod *=arr[i];
+
+        }
+        return prod;
     }
 }
